541e4506124c05bef8b7d3694f966c1aceb37063
mcarrington1 <mcarrington1@live.maryville.edu>
2021-03-25 17:27:15 -0500

    mail sending implemented w/ end point

--- src/main/java/com/image/portal/api/ShareApiDelegateImpl.java
+++ src/main/java/com/image/portal/api/ShareApiDelegateImpl.java
@@ -1,9 +1,14 @@
 package com.image.portal.api;
 
 import com.image.portal.model.ImagePortalUser;
+import com.image.portal.model.Share;
 import com.image.portal.repository.ImagePortalUserRepository;
 import com.image.portal.service.ImagePortalUserService;
+import com.image.portal.service.MailService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mail.MailException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,5 +18,19 @@ public class ShareApiDelegateImpl implements ShareApiDelegate {
     private ImagePortalUserRepository imagePortalUserRepository;
     private ImagePortalUserService imagePortalUserService = new ImagePortalUserService();
     private ImagePortalUser updatedImagePortalUser;
+
+    @Autowired
+    private MailService mailService;
+
+    @Override
+    public ResponseEntity<Share> shareImages(Share body) {
+        try {
+            mailService.sendEmail(body);
+        } catch (MailException mailException) {
+            System.out.println(mailException);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<>(body, HttpStatus.OK);
+    }
 
 }
