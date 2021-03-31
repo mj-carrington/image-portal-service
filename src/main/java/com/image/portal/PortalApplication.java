cfbed90947954c934a8dd95e665a742b6f7f4a65
mcarrington1 <mcarrington1@live.maryville.edu>
2021-03-30 21:38:58 -0500

    fixed CORS issue

--- src/main/java/com/image/portal/PortalApplication.java
+++ src/main/java/com/image/portal/PortalApplication.java
@@ -4,6 +4,9 @@ import com.image.portal.repository.ImagePortalUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 //@Import(MongoDbConfig.class)
 @SpringBootApplication
@@ -14,6 +17,16 @@ public class PortalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PortalApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
 	}
 
 }
